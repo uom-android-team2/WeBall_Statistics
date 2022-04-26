4271807c1ec0e7a2dfa85a0859728363472fbe69
DESKTOP-J7GS40K\USER <leopepp4@gmail.com>
2022-04-26 15:28:46 +0300

    Base Project - Initial commit

---
+++ WeBall-Statistics-Mobile/app/src/main/java/uom/team2/weballstatistics/MainActivity.java
@@ -0,0 +1,14 @@
+package uom.team2.weballstatistics;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

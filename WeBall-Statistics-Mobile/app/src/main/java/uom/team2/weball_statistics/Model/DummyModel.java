b46238ffa6c6c966eb542105efc68ba73ca0b883
DESKTOP-J7GS40K\USER <leopepp4@gmail.com>
2022-04-27 02:28:49 +0300

    added getters and setters to dummy model just to be clear

--- WeBall-Statistics-Mobile/app/src/main/java/uom/team2/weball_statistics/Model/DummyModel.java
+++ WeBall-Statistics-Mobile/app/src/main/java/uom/team2/weball_statistics/Model/DummyModel.java
@@ -14,4 +14,28 @@ public class DummyModel {
         this.email = email;
     }
 
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }

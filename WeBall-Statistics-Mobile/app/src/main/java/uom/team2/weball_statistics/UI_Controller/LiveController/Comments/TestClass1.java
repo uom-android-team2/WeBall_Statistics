431a8d2d87deec3425caaef020926a672992adb6
MINAS\User <ics20072@uom.edu.gr>
2022-04-30 05:06:54 +0300

    2 folders created

---
+++ WeBall-Statistics-Mobile/app/src/main/java/uom/team2/weball_statistics/UI_Controller/LiveController/Comments/TestClass1.java
@@ -0,0 +1,4 @@
+package uom.team2.weball_statistics.UI_Controller.LiveController.Comments;
+
+public class TestClass1 {
+}
---
+++ WeBall-Statistics-Mobile/app/src/main/java/uom/team2/weball_statistics/UI_Controller/LiveController/Progress/TestClass2.java
@@ -0,0 +1,4 @@
+package uom.team2.weball_statistics.UI_Controller.LiveController.Progress;
+
+public class TestClass2 {
+}

7c305ba1ffdf3f198233c3a5b353c6a4b94a9622
evitadasy <dasyevita@gmail.com>
2022-05-14 19:27:28 +0300

    some changes

--- WeBall-Statistics-Mobile/app/src/main/java/uom/team2/weball_statistics/UI_Controller/CompletedStatistics/Players/TopPlayer.java
+++ WeBall-Statistics-Mobile/app/src/main/java/uom/team2/weball_statistics/UI_Controller/CompletedStatistics/Players/TopPlayer.java
@@ -61,6 +61,6 @@ public class TopPlayer extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_top_player, container, false);
+        return inflater.inflate(R.layout.fragment_top_player_layout, container, false);
     }
 }

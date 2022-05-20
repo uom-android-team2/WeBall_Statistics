64d10780181775394e7530e1f8751f3751f31599
DESKTOP-D627JEQ\Omerta <sofiordanou@gmail.com>
2022-05-20 13:50:51 +0300

    add package R1 and button in fragmnet home Home

---
+++ WeBall-Statistics-Mobile/app/src/main/java/uom/team2/weball_statistics/UI_Controller/R1/administrator_screen1.java
@@ -0,0 +1,14 @@
+package uom.team2.weball_statistics.UI_Controller.R1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class administrator_screen1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       // setContentView(R.layout.activity_administrator_screen1);
+    }
+}

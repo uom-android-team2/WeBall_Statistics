184aa6d1e91936b7927d3f2ef843ce0ac4c5f718
Evita Dasyra <dasyevita@gmail.com>
2022-05-23 15:35:22 +0300

    fix leadersStats layout

--- WeBall-Statistics-Mobile/app/src/main/java/uom/team2/weball_statistics/UI_Controller/CompletedStatistics/Leaders/StatTitle.java
+++ WeBall-Statistics-Mobile/app/src/main/java/uom/team2/weball_statistics/UI_Controller/CompletedStatistics/Leaders/StatTitle.java
@@ -1,4 +1,4 @@
-package uom.team2.weball_statistics;
+package uom.team2.weball_statistics.UI_Controller.CompletedStatistics.Leaders;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import uom.team2.weball_statistics.R;
 
 /**
  * A simple {@link Fragment} subclass.

19c2c6d729719f2dfd65390c84a5d9017a453669
DESKTOP-D627JEQ\Omerta <sofiordanou@gmail.com>
2022-05-23 23:18:58 +0300

    add classes: fragment_date_of_match,  fragment_upcoming_matches,  fragment_previous_matches, fragment_admin_screen

--- WeBall-Statistics-Mobile/app/src/main/java/uom/team2/weball_statistics/UI_Controller/R1/fragment_upcoming_matches.java
+++ WeBall-Statistics-Mobile/app/src/main/java/uom/team2/weball_statistics/UI_Controller/R1/fragment_upcoming_matches.java
@@ -12,10 +12,10 @@ import uom.team2.weball_statistics.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link fragment_live_match_layout#newInstance} factory method to
+ * Use the {@link fragment_upcoming_matches#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class fragment_live_match_layout extends Fragment {
+public class fragment_upcoming_matches extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,7 +26,7 @@ public class fragment_live_match_layout extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public fragment_live_match_layout() {
+    public fragment_upcoming_matches() {
         // Required empty public constructor
     }
 
@@ -36,11 +36,11 @@ public class fragment_live_match_layout extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment fragment_live_match_layout.
+     * @return A new instance of fragment fragment_upcoming_matches.
      */
     // TODO: Rename and change types and number of parameters
-    public static fragment_live_match_layout newInstance(String param1, String param2) {
-        fragment_live_match_layout fragment = new fragment_live_match_layout();
+    public static fragment_upcoming_matches newInstance(String param1, String param2) {
+        fragment_upcoming_matches fragment = new fragment_upcoming_matches();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -61,6 +61,6 @@ public class fragment_live_match_layout extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_match_layout, container, false);
+        return inflater.inflate(R.layout.fragment_upcoming_matches, container, false);
     }
 }

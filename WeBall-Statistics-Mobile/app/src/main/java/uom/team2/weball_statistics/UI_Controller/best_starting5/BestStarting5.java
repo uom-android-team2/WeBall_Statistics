abe07ee3dfdba981691fd9afb13041d7038f2a5d
DESKTOP-DF467M0\Money Maker <dionisisloug@gmail.com>
2022-05-24 01:13:23 +0300

    Addition of "createBestPlayerPerPositionLayout" in BestStarting5 class so that the best_player_per_position layout can be changed programatically !-Logo image and Player photo are of type int and need to be chanegd-!

--- WeBall-Statistics-Mobile/app/src/main/java/uom/team2/weball_statistics/UI_Controller/best_starting5/BestStarting5.java
+++ WeBall-Statistics-Mobile/app/src/main/java/uom/team2/weball_statistics/UI_Controller/best_starting5/BestStarting5.java
@@ -4,9 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.fragment.app.*;
 
+import uom.team2.weball_statistics.R;
 import uom.team2.weball_statistics.databinding.FragmentBestStarting5Binding;
 
 public class BestStarting5 extends Fragment {
@@ -35,5 +38,32 @@ public class BestStarting5 extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    public static View createBestPlayerPerPositionLayout(Fragment fragment,int logo,
+                                                         int playerPhoto,
+                                                         String cityNameNumberPosition,
+                                                         String firstnameLastname,int plusMinusValue){
+        //Deriving View from layout fragment
+        View bestPlayerPerPositionLayout = fragment.getLayoutInflater().inflate(R.layout.best_player_per_position_layout,null);
+        //Changing View data based on input
+            //City-Team name-Number-Position field
+            TextView newCityNameNumberPosition = bestPlayerPerPositionLayout.findViewById(R.id.cityTeamNumberPosition);
+            newCityNameNumberPosition.setText(cityNameNumberPosition);
+            //Firstname Lastname field
+            TextView newFirstnameLastname = bestPlayerPerPositionLayout.findViewById(R.id.firstnameLastname);
+            newFirstnameLastname.setText(firstnameLastname);
+            //Plus minus value field
+            TextView newPlusMinusValue = bestPlayerPerPositionLayout.findViewById(R.id.plusMinusValue);
+            String plusMinusValueString = Integer.toString(plusMinusValue);
+            newPlusMinusValue.setText(plusMinusValueString);
+            //Logo field
+            ImageButton newLogo = bestPlayerPerPositionLayout.findViewById(R.id.logoImage);
+            newLogo.setBackgroundResource(logo); //logo is of type int
+            //Player photo field
+            ImageButton newPlayerPhoto = bestPlayerPerPositionLayout.findViewById(R.id.playerImage);
+            newPlayerPhoto.setBackgroundResource(playerPhoto); //playerPhoto is of type int
+
+        return bestPlayerPerPositionLayout;
     }
 }

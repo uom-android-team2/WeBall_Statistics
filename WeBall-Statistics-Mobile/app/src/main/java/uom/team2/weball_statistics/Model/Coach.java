c9040c402f51e34e3b9d2f777dbbf4d9b60c9564
MINAS\User <ics20072@uom.edu.gr>
2022-05-23 15:35:05 +0300

    Model classes initialized

---
+++ WeBall-Statistics-Mobile/app/src/main/java/uom/team2/weball_statistics/Model/Coach.java
@@ -0,0 +1,70 @@
+package uom.team2.weball_statistics.Model;
+
+/*
+ * @author Minas - Theodoros Charakopoulos ics20072
+ */
+public class Coach {
+    private int id;
+    private int coachedTeamID; //The obj Team that he coaching
+    private String firstname;
+    private String surname;
+    private boolean isHeadCoach;
+    private String imagePath;
+
+    public Coach(int id, int coachedTeamID, String firstname, String surname, boolean isHeadCoach, String imagePath) {
+        this.id = id;
+        this.coachedTeamID = coachedTeamID;
+        this.firstname = firstname;
+        this.surname = surname;
+        this.isHeadCoach = isHeadCoach;
+        this.imagePath = imagePath;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCoachedTeamID() {
+        return coachedTeamID;
+    }
+
+    public void setCoachedTeamID(int coachedTeamID) {
+        this.coachedTeamID = coachedTeamID;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public boolean isHeadCoach() {
+        return isHeadCoach;
+    }
+
+    public void setHeadCoach(boolean headCoach) {
+        isHeadCoach = headCoach;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+}
---
+++ WeBall-Statistics-Mobile/app/src/main/java/uom/team2/weball_statistics/Model/Match.java
@@ -0,0 +1,74 @@
+package uom.team2.weball_statistics.Model;
+
+import java.sql.Date;
+import java.util.ArrayList;
+
+/*
+ * @author Minas - Theodoros Charakopoulos ics20072
+ */
+public class Match {
+    private int id; //unique id for every match
+    private Team teamLandlord;
+    private Team guest;
+    private Date matchDate;
+    private Status status; // UPCOMING, ONGOING, COMPLETED
+    private ArrayList<Action> actions; //Class Action implemented in other branch
+    //Will added fields specified for comments and progress to appear them after the match completed
+
+    public Match(int id, Team teamLandlord, Team guest, Date matchDate, Status status) {
+        this.id = id;
+        this.teamLandlord = teamLandlord;
+        this.guest = guest;
+        this.matchDate = matchDate;
+        this.status = status;
+        actions = new ArrayList<Action>();
+    }
+
+    public void addAction(Action action) {
+        this.actions.add(action);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Team getTeamLandlord() {
+        return teamLandlord;
+    }
+
+    public void setTeamLandlord(Team teamLandlord) {
+        this.teamLandlord = teamLandlord;
+    }
+
+    public Team getGuest() {
+        return guest;
+    }
+
+    public void setGuest(Team guest) {
+        this.guest = guest;
+    }
+
+    public Date getMatchDate() {
+        return matchDate;
+    }
+
+    public void setMatchDate(Date matchDate) {
+        this.matchDate = matchDate;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public ArrayList<Action> getActions() {
+        return actions;
+    }
+}

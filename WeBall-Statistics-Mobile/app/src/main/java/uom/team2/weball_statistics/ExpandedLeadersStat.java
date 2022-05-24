3fc0139a34db7487245459ad9f5520c76ae5ef77
Evita Dasyra <dasyevita@gmail.com>
2022-05-24 22:49:45 +0300

    create fr_expanded_leaders_statistic and add listeners to each button in fr_leaders_stats

--- WeBall-Statistics-Mobile/app/src/main/java/uom/team2/weball_statistics/ExpandedLeadersStat.java
+++ WeBall-Statistics-Mobile/app/src/main/java/uom/team2/weball_statistics/ExpandedLeadersStat.java
@@ -8,11 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ExpandedLeadersStat#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class ExpandedLeadersStat extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match

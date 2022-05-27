4494331cfbf367ba766a94ec857b63bd08c04bca
Panagiotis Machairas <ics20044@uom.edu.gr>
2022-05-28 02:38:09 +0300

    New changes

--- WeBall-Statistics-Mobile/app/src/main/java/uom/team2/weball_statistics/MainActivity.java
+++ WeBall-Statistics-Mobile/app/src/main/java/uom/team2/weball_statistics/MainActivity.java
@@ -36,8 +36,7 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        Intent i = new Intent(this, WelcomeScreenActivity.class);
-        startActivity(i);
+
     }
 
     @Override

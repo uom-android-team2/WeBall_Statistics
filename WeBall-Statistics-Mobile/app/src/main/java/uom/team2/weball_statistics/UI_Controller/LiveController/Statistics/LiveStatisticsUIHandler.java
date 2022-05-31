b366d7fff45e721252a5c33f8434317feb1c6e0b
DESKTOP-I3LOKU1\leop <leopepp4@gmail.com>
2022-05-31 19:01:53 +0300

    changes are made on ui thread

--- WeBall-Statistics-Mobile/app/src/main/java/uom/team2/weball_statistics/UI_Controller/LiveController/Statistics/LiveStatisticsUIHandler.java
+++ WeBall-Statistics-Mobile/app/src/main/java/uom/team2/weball_statistics/UI_Controller/LiveController/Statistics/LiveStatisticsUIHandler.java
@@ -61,30 +61,41 @@ public class LiveStatisticsUIHandler {
         });
     }
 
-    public static void updateProgressBarLayoutTeam1(HashMap<String, View> mapOfProgressBarLayout, LiveStatistics key, int max, int value){
+    public static void updateProgressBarLayoutTeam1(Fragment fragment, HashMap<String, View> mapOfProgressBarLayout, LiveStatistics key, int max, int value){
         View view =  mapOfProgressBarLayout.get(key.name());
         TextView statisticProgress = view.findViewById(R.id.team1_progress_text);
-        statisticProgress.setText(String.valueOf(value));
         ProgressBar progressBar = view.findViewById(R.id.team1_progressbar);
-        progressBar.setMax(max);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            progressBar.setProgress(value, true);
-        }else{
-            progressBar.setProgress(value);
-        }
+        fragment.requireActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                progressBar.setMax(max);
+                statisticProgress.setText(String.valueOf(value));
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    progressBar.setProgress(value, true);
+                }else{
+                    progressBar.setProgress(value);
+                }
+
+            }
+        });
     }
 
-    public static void updateProgressBarLayoutTeam2(HashMap<String, View> mapOfProgressBarLayout, LiveStatistics key, int max, int value){
+    public static void updateProgressBarLayoutTeam2(Fragment fragment, HashMap<String, View> mapOfProgressBarLayout, LiveStatistics key, int max, int value){
         View view =  mapOfProgressBarLayout.get(key.name());
         TextView statisticProgress = view.findViewById(R.id.team2_progress_text);
-        statisticProgress.setText(String.valueOf(value));
         ProgressBar progressBar = view.findViewById(R.id.team2_progressbar);
-        progressBar.setMax(max);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            progressBar.setProgress(value, true);
-        }else{
-            progressBar.setProgress(value);
-        }
+        fragment.requireActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                progressBar.setMax(max);
+                statisticProgress.setText(String.valueOf(value));
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    progressBar.setProgress(value, true);
+                }else{
+                    progressBar.setProgress(value);
+                }
+            }
+        });
     }
 
 

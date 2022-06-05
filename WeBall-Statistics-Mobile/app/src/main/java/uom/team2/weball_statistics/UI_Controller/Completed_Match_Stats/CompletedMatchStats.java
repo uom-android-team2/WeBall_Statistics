8b6be609fb897b64919e906a248f6265fa799dab
ASEREPONHRE\George <ics20051@uom.edu.gr>
2022-06-05 20:38:33 +0300

    Final Version R6

---
+++ WeBall-Statistics-Mobile/app/src/main/java/uom/team2/weball_statistics/UI_Controller/Completed_Match_Stats/CompletedMatchStats.java
@@ -0,0 +1,41 @@
+package uom.team2.weball_statistics.UI_Controller.Completed_Match_Stats;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import uom.team2.weball_statistics.databinding.CompletedMatchStatsBinding;
+
+public class CompletedMatchStats extends Fragment {
+
+    private CompletedMatchStatsBinding binding;
+
+    public CompletedMatchStats() {
+    }
+
+    public static uom.team2.weball_statistics.UI_Controller.Completed_Match_Stats.CompletedMatchStats getInstance() {
+        return new uom.team2.weball_statistics.UI_Controller.Completed_Match_Stats.CompletedMatchStats();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = CompletedMatchStatsBinding.inflate(inflater, container, false);
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}

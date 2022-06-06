808ebb2133e11590b148a470f693037127f5ab76
MINAS\User <ics20072@uom.edu.gr>
2022-06-06 16:43:54 +0300

    replace match with action class in DAOAction class

--- WeBall-Statistics-Mobile/app/src/main/java/uom/team2/weball_statistics/Service/DAOAction.java
+++ WeBall-Statistics-Mobile/app/src/main/java/uom/team2/weball_statistics/Service/DAOAction.java
@@ -7,9 +7,8 @@ import com.google.firebase.database.FirebaseDatabase;
 import java.util.HashMap;
 
 import uom.team2.weball_statistics.Model.Actions.Action;
-import uom.team2.weball_statistics.Model.Match;
 
-public class DAOAction implements DAOCRUDService <Match> {
+public class DAOAction implements DAOCRUDService <Action> {
 
     private DatabaseReference databaseReference;
     public static DAOAction instance = new DAOAction();
@@ -28,32 +27,32 @@ public class DAOAction implements DAOCRUDService <Match> {
     }
 
     @Override
-    public Task<Void> insert(Match data) {
+    public Task<Void> insert(Action data) {
         return null;
     }
 
     @Override
-    public Task<Void> update(HashMap<String, Match> data) {
+    public Task<Void> update(HashMap<String, Action> data) {
         return null;
     }
 
     @Override
-    public Task<Void> delete(Match data) {
+    public Task<Void> delete(Action data) {
         return null;
     }
 
     @Override
-    public Match get() {
+    public Action get() {
         return null;
     }
 
     @Override
-    public Match get(Match data) {
+    public Action get(Action data) {
         return null;
     }
 
     @Override
-    public void update(Match data) {
+    public void update(Action data) {
 
     }
 }

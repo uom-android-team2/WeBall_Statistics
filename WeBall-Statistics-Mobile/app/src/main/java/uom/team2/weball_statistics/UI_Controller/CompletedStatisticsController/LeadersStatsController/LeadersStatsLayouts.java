ab087905e1d45e08a02e6250d90b943f594f1840
evitadasy <dasyevita@gmail.com>
2022-06-07 21:33:55 +0300

    some changes

--- WeBall-Statistics-Mobile/app/src/main/java/uom/team2/weball_statistics/UI_Controller/CompletedStatisticsController/LeadersStatsController/LeadersStatsLayouts.java
+++ WeBall-Statistics-Mobile/app/src/main/java/uom/team2/weball_statistics/UI_Controller/CompletedStatisticsController/LeadersStatsController/LeadersStatsLayouts.java
@@ -1,18 +1,33 @@
 package uom.team2.weball_statistics.UI_Controller.CompletedStatisticsController.LeadersStatsController;
 
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
-
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.fragment.app.Fragment;
 import uom.team2.weball_statistics.R;
+import uom.team2.weball_statistics.UI_Controller.CompletedStatisticsController.LeadersStats;
 
 public class LeadersStatsLayouts {
 
-    public static View createPlayersStatsLayout(Fragment fragment, String name){
-        View playersStatslayout = fragment.getLayoutInflater().inflate(R.layout.fragment_players_stats_layout, null);
-        TextView statsName = playersStatslayout.findViewById(R.id.TITLE);
-        statsName.setText(name);
-        return playersStatslayout;
-    }
+
+
+
+//    public static View createPlayersStatsLayout(Fragment fragment, String name){
+//        View playersStatslayout = fragment.getLayoutInflater().inflate(R.layout.fragment_players_stats_layout, null);
+//        TextView statsName = playersStatslayout.findViewById(R.id.TITLE);
+//        statsName.setText(name);
+////        ////
+////        Fragment LeadersStatsFragment = fragment.getParentFragment();
+////        ImageButton button = playersStatslayout.findViewById(R.id.button);
+////        button.setOnClickListener(e -> {
+////                NavHostFragment.findNavController(LeadersStatsFragment).navigate(R.id.action_sharedTabContainer_to_expandedLeadersStat); });
+//        ////
+//        return playersStatslayout;
+//    }
+
+
 }

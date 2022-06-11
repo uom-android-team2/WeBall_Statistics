581477c7b16f51d9ccb41349cf1288bb907621a9
MINAS\User <ics20072@uom.edu.gr>
2022-06-11 14:19:56 +0300

    small changes

--- WeBall-Statistics-Mobile/app/src/main/java/uom/team2/weball_statistics/configuration/Config.java
+++ WeBall-Statistics-Mobile/app/src/main/java/uom/team2/weball_statistics/configuration/Config.java
@@ -4,6 +4,8 @@ public class Config {
 
     public static final String IP = "192.168.56.1";
     public static final String API_URL = "http://" + IP + "/WeBall_Statistics-Backend/API/";
+    //We will use the format bellow for the apis.
+    public static final String MATCHES_API = "http://" + IP + "/weBall_Statistics-Backend/API/match.php/";
     public static final String API_REFEREE = "referee.php";
     public static final String API_PLAYER_STATS_LIVE = "playerLiveStatistics.php";
     public static final String API_TEAM_STATS_LIVE = "teamLiveStatistics.php";
@@ -13,6 +15,7 @@ public class Config {
     public static final String TEAM_ID = "team_id";
     public static final String PLAYER_IMAGES_RESOURCES = "http://" + IP + "/WeBall_Statistics-Backend/resources/player_images/";
     public static final String TEAM_IMAGES_RESOURCES = "http://" + IP + "/WeBall_Statistics-Backend/resources/team_images/";
+
     public static final int COEFFICIENT_WIN = 2;
     public static final int COEFFICIENT_LOOSE = 1;
     public static final int COEFFICIENT_THREE_POINT = 3;

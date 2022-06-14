dc579da57fd0bdd3124351742de87fe9b60fd420
MINAS\User <ics20072@uom.edu.gr>
2022-06-14 16:42:14 +0300

    add one more field to Action class

--- WeBall-Statistics-Mobile/app/src/main/java/uom/team2/weball_statistics/Model/Actions/Action.java
+++ WeBall-Statistics-Mobile/app/src/main/java/uom/team2/weball_statistics/Model/Actions/Action.java
@@ -4,14 +4,16 @@ public class Action {
     protected String actionDesc;
     protected String timeHappened;
     protected BelongsTo belongsTo;
+    protected String imageActionPath;
 
     public Action(){
         //Empty constructor required
     }
 
-    public Action (String timeHappened, BelongsTo belongsTo) {
+    public Action (String timeHappened, BelongsTo belongsTo, String imageActionPath) {
         this.timeHappened = timeHappened;
         this.belongsTo = belongsTo;
+        this.imageActionPath = imageActionPath;
     }
 
     //Method that will set the value that we want to appear to the action ui

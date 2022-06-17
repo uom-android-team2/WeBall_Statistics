cf4369addb6729e30e2a78cdbc2cee1ea2e94f9b
DESKTOP-I3LOKU1\leop <leopepp4@gmail.com>
2022-06-17 21:55:11 +0300

    admins view done button ready

--- WeBall-Statistics-Mobile/app/src/main/java/uom/team2/weball_statistics/UI_Controller/AdminsView/AdminsView.java
+++ WeBall-Statistics-Mobile/app/src/main/java/uom/team2/weball_statistics/UI_Controller/AdminsView/AdminsView.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import com.squareup.picasso.Picasso;
 
@@ -263,8 +264,9 @@ public class AdminsView extends Fragment {
             @Override
             public void onClick(View view) {
                 binding.clock.stop();
-                keepClock = SystemClock.elapsedRealtime() - binding.clock.getBase();
-                running = false;
+                Bundle bundle = new Bundle();
+                bundle.putBoolean("isAdmin", true);
+                NavHostFragment.findNavController(AdminsView.this).navigate(R.id.action_adminsView_to_matchesTabContainer, bundle);
             }
         });
 
